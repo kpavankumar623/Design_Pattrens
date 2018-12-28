7b4f798968f05cd813e24c6a68c1d129ce6c3c37
pavan kumar <kpavankumar623@hotmail.com>
2018-12-28 12:48:01 +0530

    commiting to the github..

--- src/FactoryMethod/FactoryPlan.java
+++ src/FactoryMethod/FactoryPlan.java
@@ -1,17 +1,17 @@
 package FactoryMethod;
 
 public class FactoryPlan {
-	public Plan getPlan(String planType) {
-		if (planType == null)
-			return null;
-		if (planType.equalsIgnoreCase("DOMENSTICPLAN")) {
-			return new DomensticPlan();
-		} else if (planType.equalsIgnoreCase("COMMERCIALPLAN")) {
-			return new CommercialPlan();
-		} else if (planType.equalsIgnoreCase("INSTITUTIONALPLAN")) {
-			return new InstitutionalPlan();
-		}
-		return null;
 
+	public Plan getPlan(String planType) {
+		switch(planType) {
+		case "DomensticPlan" :
+			return new DomensticPlan();
+		case "CommercialPlan":
+			return new CommercialPlan();
+		case "InstitutionalPlan":
+			return new InstitutionalPlan();
+		default:
+		return null;
+		}
 	}
 }
